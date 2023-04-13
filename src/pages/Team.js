bcfc1e1b4cd8ca1519569862f7b2cfff5a0f5104
OlaniyiHope <olaniyihoppee@gmail.com>
2023-04-13 18:04:28 +0100

    added teams

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import img1 from "./images/book.avif.png"
-import img2 from "./images/sus.avif.png"
-import img3 from "./images/work.avif.png"
+import img3 from "./matt2.jpg"
+import img2 from "./will4.jpg"
+import img1 from "./dave2.jpg"
 import "./team.css"
 const Team = () => {
 
@@ -21,10 +21,7 @@ const Team = () => {
           <div className="myCarousel">
             <h3>David Oriazowan </h3>
             <h4>Project Manager</h4>
-            <p>
-              It's freeing to be able to catch up on customized news and not be
-              distracted by a social media element on the same site
-            </p>
+         
           </div>
         </div>
 
@@ -33,10 +30,7 @@ const Team = () => {
           <div className="myCarousel">
             <h3>Will Forbes</h3>
             <h4>Managing Director</h4>
-            <p>
-              The simple and intuitive design makes it easy for me use. I highly
-              recommend Fetch to my peers.
-            </p>
+       
           </div>
         </div>
 
@@ -45,10 +39,7 @@ const Team = () => {
           <div className="myCarousel">
             <h3>Nathan Marriot</h3>
             <h4>Creative Director</h4>
-            <p>
-              I enjoy catching up with Fetch on my laptop, or on my phone when
-              I'm on the go!
-            </p>
+           
           </div>
         </div>
       </Carousel>
