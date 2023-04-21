266504281ce15a2c647275dc3e3bbc53d1693288
OlaniyiHope <olaniyihoppee@gmail.com>
2023-04-21 02:15:13 +0100

    all

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -147,21 +147,16 @@ Book A Free Strategy Call With Our Team
         
 
             <ul class="contact-social">
-                <li>
-                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a>
-                </li>
-                <li>
-                    <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
-                </li>
+            <li>
+            <a href="https://www.linkedin.com/company/lightwave-creative-agency"><i class="fa fa-linkedin"></i></a>
+        </li>
+        <li>
+            <a href="https://instagram.com/lightwavecreative.io"><i class="fa fa-instagram"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fa fa-facebook"></i></a>
+      </li>
+              
             </ul> 
 
         </div> 
