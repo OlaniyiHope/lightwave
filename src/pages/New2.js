9eff437b4e1b9038248588bf3a3896c2cc331162
OlaniyiHope <olaniyihoppee@gmail.com>
2023-04-30 16:51:42 +0100

    all

--- src/pages/New2.js
+++ src/pages/New2.js
@@ -1,10 +1,10 @@
 import React from "react"
 export const New2 = () => {
   return (
-    <div>
+    <>
 
     <video class="jet-video-player jet-video-html5-player" src="https://res.cloudinary.com/dftygokow/video/upload/v1682846161/56_bacts3.mp4" autoplay="" loop="" playsinline="" muted=""></video> 
     
-    </div>
+    </>
   )
 }
