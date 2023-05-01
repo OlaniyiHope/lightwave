b9e225a6929effcfeafeccab01661bddac9b129b
OlaniyiHope <olaniyihoppee@gmail.com>
2023-05-01 15:43:48 +0100

    all

--- src/pages/New2.js
+++ src/pages/New2.js
@@ -3,8 +3,8 @@ export const New2 = () => {
   return (
     <div>
 
-    <video  style={{width: "100%", height: "500px"}} src="https://res.cloudinary.com/dftygokow/video/upload/v1682846161/56_bacts3.mp4" ></video> 
-    
+    <video autoplay loop muted src="https://res.cloudinary.com/dftygokow/video/upload/v1682846161/56_bacts3.mp4"></video>
+   
     </div>
   )
 }
